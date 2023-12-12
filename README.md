1e216b686077269fd138799e7da5fbf22b589812
Gabor Szarnyas <szarnyasg@gmail.com>
2023-12-12 21:51:03 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Graphalytics results
 
 Scripts to compute the results of the Graphalytics 2023 competition.
+
+```bash
+python3 analyze.py
+```
--- analyze.py
+++ analyze.py
@@ -2,29 +2,71 @@ import json
 import glob
 import duckdb
 
-with open("results.csv", "w") as of:
-    of.write("platform,job,algorithm,dataset,run,success,load_time,makespan,processing_time\n")
+con = duckdb.connect("gx-results.duckdb")
+con.sql("""CREATE OR REPLACE TABLE datasets   AS FROM read_csv_auto('datasets.csv',   header = true);""")
+con.sql("""CREATE OR REPLACE TABLE algorithms AS FROM read_csv_auto('algorithms.csv', header = true);""")
+con.sql("""
+    CREATE OR REPLACE TABLE results (
+        platform VARCHAR,
+        job VARCHAR,
+        algorithm VARCHAR,
+        dataset VARCHAR,
+        run VARCHAR,
+        success BOOLEAN,
+        load_time FLOAT,
+        makespan FLOAT,
+        processing_time FLOAT
+    );
+    """)
 
-    for path in glob.glob("*.json"):
-        with open(path) as f:
-            j = json.load(f)
-            platform = j["system"]["platform"]["name"]
+for path in glob.glob("submissions/**/*.json", recursive=True):
+    with open(path) as f:
+        j = json.load(f)
+        platform = j["system"]["platform"]["name"]
 
+        for job_id in j["result"]["jobs"]:
+            job = j["result"]["jobs"][job_id]
+            algorithm = job["algorithm"]
+            dataset = job["dataset"]
+            runs = job["runs"]
 
-            for job_id in j["result"]["jobs"]:
-                job = j["result"]["jobs"][job_id]
-                algorithm = job["algorithm"]
-                dataset = job["dataset"]
-                runs = job["runs"]
+            for run_id in runs:
+                run = j["result"]["runs"][run_id]
 
-                for run_id in runs:
-                    run = j["result"]["runs"][run_id]
+                success = run["success"]
+                load_time = run["load_time"]
+                makespan = run["makespan"]
+                processing_time = run["processing_time"]
 
-                    success = run["success"]
-                    load_time = run["load_time"]
-                    makespan = run["makespan"]
-                    processing_time = run["processing_time"]
+                con.sql(f"INSERT INTO results VALUES ('{platform}', '{job_id}', '{algorithm}', '{dataset}', '{run_id}', {success}, {load_time}, {makespan}, {processing_time});")
 
-                    of.write(f"{platform},{job_id},{algorithm},{dataset},{run_id},{success},{load_time},{makespan},{processing_time}\n")
+con.sql("""COPY results TO 'results.csv';""")
 
-# TODO: Pivot
+con.sql("""
+    CREATE OR REPLACE TABLE results AS
+        SELECT * EXCLUDE name
+        FROM results
+        JOIN datasets ON datasets.name = results.dataset;
+    """)
+
+con.sql("""
+    CREATE OR REPLACE TABLE results AS
+        SELECT *
+        FROM results
+        WHERE success = true
+          AND size IN ('S', 'M', 'L', 'XL', '2XL', '3XL');
+    """)
+
+con.sql("""
+    CREATE OR REPLACE TABLE results AS
+        SELECT platform, algorithm, dataset, size, mean(processing_time) AS time
+        FROM results
+        GROUP BY platform, algorithm, dataset, size;
+    """)
+
+con.sql("""
+    CREATE OR REPLACE TABLE results_wide AS
+        PIVOT results
+        ON algorithm
+        USING sum(time);
+    """)
