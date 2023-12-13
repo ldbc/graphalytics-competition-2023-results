5ac531d5446767ebc9e4f9071d0864f0aaa0a981
Gabor Szarnyas <szarnyasg@gmail.com>
2023-12-13 21:48:38 +0100

    work on analysis script

--- analyze.py
+++ analyze.py
@@ -6,11 +6,12 @@ con = duckdb.connect("gx-results.duckdb")
 con.sql("""CREATE OR REPLACE TABLE timeouts AS SELECT size, timeout_minutes, timeout_minutes * 60 AS timeout_seconds FROM read_csv_auto('timeouts.csv', header = true);""")
 con.sql("""CREATE OR REPLACE TABLE algorithms AS FROM read_csv_auto('algorithms.csv', header = true);""")
 con.sql("""CREATE OR REPLACE TABLE datasets AS FROM read_csv_auto('datasets.csv', header = true);""")
-con.sql("""CREATE OR REPLACE TABLE platforms AS FROM read_csv_auto('platforms.csv', header = true);""")
 con.sql("""CREATE OR REPLACE TABLE size_ordering AS FROM read_csv_auto('size_ordering.csv');""")
 con.sql("""
-    CREATE OR REPLACE TABLE results (
+    CREATE OR REPLACE TABLE results_raw (
         platform VARCHAR,
+        environment_name VARCHAR,
+        pricing DOUBLE,
         job VARCHAR,
         algorithm VARCHAR,
         dataset VARCHAR,
@@ -29,7 +30,10 @@ for path in glob.glob("submissions/**/*.json", recursive=True):
         pricing = j["system"]["pricing"]
         environment_name = j["system"]["environment"]["name"]
 
-        print(f"{platform}: ${pricing}, env name {environment_name}")
+        if platform == 'GraphBLAS Intel Xeon Gold 6342':
+            pricing = 15354.81
+        if environment_name == 'ecs.c8i.24xlarge':
+            pricing = 100_000_000.00
 
         for job_id in j["result"]["jobs"]:
             job = j["result"]["jobs"][job_id]
@@ -48,34 +52,57 @@ for path in glob.glob("submissions/**/*.json", recursive=True):
                 if platform == "GraphBLAS":
                     platform = "GraphBLAS Intel Xeon Platinum 8369"
 
-                con.sql(f"INSERT INTO results VALUES ('{platform}', '{job_id}', '{algorithm}', '{dataset}', '{run_id}', {success}, {load_time}, {makespan}, {processing_time});")
+                con.sql(f"INSERT INTO results_raw VALUES ('{platform}', '{environment_name}', {pricing}, '{job_id}', '{algorithm}', '{dataset}', '{run_id}', {success}, {load_time}, {makespan}, {processing_time});")
 
 #con.sql("""COPY results TO 'results.csv';""")
 
 con.sql("""
-    CREATE OR REPLACE TABLE results AS
-        SELECT * EXCLUDE name
-        FROM results
-        JOIN datasets ON datasets.name = results.dataset
+    CREATE OR REPLACE TABLE results_raw AS
+        SELECT *
+        FROM results_raw
+        JOIN datasets ON datasets.name = results_raw.dataset
     ;
     """)
 
 con.sql("""
     CREATE OR REPLACE TABLE results AS
-        SELECT *
-        FROM results
+        SELECT * EXCLUDE name
+        FROM results_raw
         WHERE success = true
           AND size IN ('S', 'M', 'L', 'XL', '2XL+')
     ;
     """)
 
+# load results
+
 con.sql("""
     CREATE OR REPLACE TABLE results AS
-        SELECT platform, algorithm, dataset, size, mean(processing_time) AS time
+        SELECT
+            platform,
+            environment_name,
+            pricing,
+            algorithm,
+            dataset,
+            size,
+            avg(makespan) AS makespan,
+            avg(processing_time) AS processing_time,
+            count(*) AS runs
         FROM results
-        GROUP BY platform, algorithm, dataset, size
+        GROUP BY ALL
+        --HAVING runs >= 3
     ;
     """)
+
+# create list of unique platform names
+
+con.sql("""
+    CREATE OR REPLACE TABLE platforms AS
+        SELECT DISTINCT platform AS name
+        FROM results
+    ;
+    """)
+
+# create the required workload for each size category
 
 con.sql("""
     CREATE OR REPLACE TABLE workload AS
@@ -87,10 +114,21 @@ con.sql("""
     ;
     """)
 
-# for systems not having a result (time == NULL), we assign 3 times the timeout as penalty
+# for systems that do not have a result (time == NULL) for a given workload (=algorithm/dataset combination),
+# we assign 3 times the timeout as penalty
+
 con.sql("""
     CREATE OR REPLACE TABLE results_full AS
-        SELECT workload.size AS size, workload.dataset AS dataset, workload.algorithm AS algorithm, workload.platform platform, coalesce(results.time, 3 * timeouts.timeout_seconds) AS time, platforms.pricing AS pricing
+        SELECT
+            workload.size AS size,
+            workload.platform AS platform,
+            results.environment_name AS environment_name,
+            results.pricing AS pricing,
+            workload.dataset AS dataset,
+            workload.algorithm AS algorithm,
+            coalesce(results.makespan, 3 * timeouts.timeout_seconds) AS makespan,
+            coalesce(results.processing_time, 3 * timeouts.timeout_seconds) AS processing_time,
+            coalesce(runs, 3) AS runs
         FROM workload
         LEFT JOIN results
                ON workload.dataset = results.dataset
@@ -105,10 +143,18 @@ con.sql("""
 
 con.sql("""
     CREATE OR REPLACE TABLE results_platform_algorithm AS
-        SELECT size, platform, algorithm, pricing, mean(time) AS time
+        SELECT
+            size,
+            platform,
+            environment_name,
+            max(pricing) AS pricing,
+            algorithm,
+            avg(makespan) AS mean_makespan,
+            avg(processing_time) AS mean_processing_time,
+            min(runs) AS min_runs_per_workload_item
         FROM results_full
         GROUP BY ALL
-        ORDER BY size ASC, algorithm ASC, time ASC
+        ORDER BY size ASC, algorithm ASC, mean_processing_time ASC
     ;
     """)
 
@@ -117,11 +163,15 @@ con.sql("""
         SELECT
             size,
             platform,
-            mean(time) AS time,
-            pricing,
-        FROM results_full
+            environment_name,
+            max(pricing) AS pricing, -- NULL prices should be consumed by max()
+            avg(mean_makespan) AS mean_makespan,
+            avg(mean_processing_time) AS mean_processing_time,
+            min(min_runs_per_workload_item) AS min_runs_per_workload_item
+        FROM results_platform_algorithm
         GROUP BY ALL
-        ORDER BY size ASC, time ASC
+        HAVING pricing IS NOT NULL
+        ORDER BY size ASC, mean_processing_time ASC
     ;
     """)
 
@@ -131,20 +181,31 @@ con.sql("""
             rank,
             size,
             platform,
-            time,
+            environment_name,
             pricing,
-            1 / time / pricing * 1000 * 1000 * 1000 AS throughput_per_dollar
+            mean_makespan,
+            mean_processing_time,
+            1 / mean_processing_time / pricing * 1000 * 1000 * 1000 AS throughput_per_dollar,
+            min(min_runs_per_workload_item) AS min_runs_per_workload_item
         FROM results_platform
         JOIN size_ordering
           ON size_ordering.name = results_platform.size
+        GROUP BY ALL
         ORDER BY rank ASC, throughput_per_dollar DESC
     ;
     """)
 
 con.sql("""
-    COPY (SELECT * EXCLUDE rank FROM results_platform_price_adjusted) TO 'results_platform_price_adjusted.csv' (SEPARATOR '\t');
-    """)
-
-con.sql("""
-    COPY (SELECT * EXCLUDE rank FROM results_platform_price_adjusted WHERE platform != 'GraphBLAS Intel Xeon Platinum 8369') TO 'results_platform_price_adjusted_3runs.csv' (SEPARATOR '\t');
+    COPY (
+        SELECT
+            rank AS "Rank",
+            size AS "Size",
+            platform AS "Platform",
+            environment_name AS "Environment name",
+            pricing AS "Pricing (USD)",
+            mean_makespan AS "Mean makespan (s)",
+            mean_processing_time AS "Mean processing time (s)",
+            throughput_per_dollar AS "Throughput per dollar",
+            min_runs_per_workload_item AS "Minimum runs per workload item"
+        FROM results_platform_price_adjusted) TO 'results_platform_price_adjusted.csv' (SEPARATOR '\t', QUOTE '');
     """)
